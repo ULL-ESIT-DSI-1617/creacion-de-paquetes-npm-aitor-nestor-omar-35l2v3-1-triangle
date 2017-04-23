69d749eb805c528f5b4c148a280e27a6d1838448
Nestor <alu0100812503@ull.edu.es>
2017-04-23 16:30:07 +0100

    Arreglos menores

--- test/test.js
+++ test/test.js
@@ -1,9 +1,9 @@
-var Triangle = require("../lib/triangle");
+let Triangle = require("../lib/triangle");
 
 describe("getArea Triangle", function() {
   it("must compute the triangle area correctly", function() {
     let a = new Triangle({ width: 100, height: 100 });
-    var result = a.getArea();
+    let result = a.getArea();
      result.should.equal(5000);
   })
 });
