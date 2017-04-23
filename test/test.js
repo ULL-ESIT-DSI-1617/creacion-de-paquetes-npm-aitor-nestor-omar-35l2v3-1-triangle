a12d3e6a0dcb89411dfaef6aad7a85b8ea22aa59
Nestor <alu0100812503@ull.edu.es>
2017-04-23 13:29:53 +0100

    Añadido pruebas

--- test/test.js
+++ test/test.js
@@ -1,9 +1,9 @@
-//TODO 
+var Triangle = require("../lib/triangle");
 
 describe("getArea Triangle", function() {
   it("must compute the triangle area correctly", function() {
     let a = new Triangle({ width: 100, height: 100 });
     var result = a.getArea();
-    result.should.match(/^5000$/);
+     result.should.equal(5000);
   })
 });
